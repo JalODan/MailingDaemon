9b9b0f3fd0f40878ccc3dd780304d442d173e50c
Olzhas Jalmukhambetov <olzhas.jalmukhambetov@gmail.com>
2023-05-09 01:33:41 +0600

    java version

--- src/main/java/kz/oj/mailingdaemon/MailingDaemonApplication.java
+++ src/main/java/kz/oj/mailingdaemon/MailingDaemonApplication.java
@@ -5,12 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
+//import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.event.EventListener;
+//import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
@@ -39,11 +39,11 @@ public class MailingDaemonApplication {
         return dataSourceProperties.initializeDataSourceBuilder().build();
     }
 
-    @EventListener
-    public void printApplicationUrl(ApplicationStartedEvent event) {
-        LoggerFactory.getLogger(MailingDaemonApplication.class).info("Application started at "
-                + "http://localhost:"
-                + environment.getProperty("local.server.port")
-                + Strings.nullToEmpty(environment.getProperty("server.servlet.context-path")));
-    }
+//    @EventListener
+//    public void printApplicationUrl(ApplicationStartedEvent event) {
+//        LoggerFactory.getLogger(MailingDaemonApplication.class).info("Application started at "
+//                + "http://localhost:"
+//                + environment.getProperty("local.server.port")
+//                + Strings.nullToEmpty(environment.getProperty("server.servlet.context-path")));
+//    }
 }
